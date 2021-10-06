bc8f1fe65de41ebc7b27657563e82aaa1106d038
Yosh1aki <kamishioiri1016@gmail.com>
2021-10-07 06:36:25 +0900

    画面追加、体裁整え

--- js/todoList.js
+++ js/todoList.js
@@ -1,8 +1,27 @@
+/**
+ * タスク一覧のリストを作成します。
+ */
 window.onload = function () {
-    var todo = ['デザインカンプ作成', 'データ整理', '勉強会申し込み', '牛乳買う'];
+    var todo = ["Task1", "Task2", "Task3", "Task4", "Task5"];
     todo.forEach(task => {
         var li = document.createElement('li');
             li.textContent = task;
-            document.getElementById('list').appendChild(li);
+            document.getElementById('todolist').appendChild(li);
     })
+}
+
+/**
+ * 登録済みのタスク一覧をファイルから取得します。
+ * 空やファイルが存在しない場合は空のコレクションを返します。
+ * @returns ファイルから読み込んだタスク一覧
+ */
+function readFile() {
+    const file_path = "../data/todolist.txt";
+    var reader = new FileReader(file_path);
+    var todo_list = [];
+    while (reader != null) {
+        var line = reader.readAsText(file_path, "UTF-8");
+        todo_list.push(reader.readAsText(file_path, "UTF-8"));
+    }
+    return todo_list;
 }
